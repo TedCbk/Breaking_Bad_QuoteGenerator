0b1d65d1b6a902e59969b4f03ff14f71b9a50bdb
LiamPorton <69626686+LiamPorton@users.noreply.github.com>
2021-02-23 18:20:59 +0100

    Typeit Js + Avatar

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function GetQuote() {
+    loading();
     const apiUrl = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
     try {
         // Work without proxy, but just in case putting it
@@ -19,7 +35,8 @@ async function GetQuote() {
         authorText.innerText = data[0].author;
         quoteText.innerText = data[0].quote;
         console.log(data);
-        
+        //Stop Loader, show quote
+        complete();
     } catch(error){
         // displaying the quote to see how much can be error
         GetQuote();
@@ -40,3 +57,9 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 GetQuote();
+
+new TypeIt('#title', {
+    strings: "Breaking Bad Quotes",
+    speed: 150,
+    loop: false,
+}).go()
